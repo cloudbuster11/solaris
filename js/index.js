c2671f67c0adcb6cbc7b74fd91abd40065629b62
Joakim Jönsson <jockejockejockejocke@gmail.com>
2022-11-28 09:14:02 +0100

    Created renderPlanetsHtml function.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 const BASE_URL = 'https://fathomless-shelf-54969.herokuapp.com';
+const planetsContainer = document.querySelector('.planets__container');
+let planetsList = [];
 
 async function getKey() {
   const response = await fetch(`${BASE_URL}/keys`, {
@@ -17,8 +19,30 @@ async function getPlanets() {
       'x-zocom': key,
     },
   });
-  const data = await response.json();
+  let data = await response.json();
+  planetsList = data;
   console.log(data);
+  renderPlanets(data);
 }
 
 getPlanets();
+
+planetsContainer.addEventListener('click', (event) => {
+  let clickedPlanetId = event.target.dataset.id;
+  displayPlanetInfo(clickedPlanetId, planetsList);
+});
+
+function renderPlanets(planets) {
+  for (let i = 0; i < planets.bodies.length; i++) {
+    // console.log(planets.bodies[i]);
+    const planetHtml = `<article class="planet planets__${planets.bodies[
+      i
+    ].name.toLowerCase()}" data-id="${planets.bodies[i].id}"></article>`;
+    planetsContainer.insertAdjacentHTML('beforeend', planetHtml);
+  }
+}
+
+function displayPlanetInfo(clickedPlanetId, planetsList) {
+  console.log(clickedPlanetId);
+  console.log(planetsList);
+}
